970d070c33a3e74d525eaa196624f78f213b1b64
stivendarsi <stivendarsi@gmail.com>
2022-03-26 08:01:10 +0300

    New Block !
    -Titanium Block

--- src/main/java/net/dubi/tutorialmod/TutorialMod.java
+++ src/main/java/net/dubi/tutorialmod/TutorialMod.java
@@ -1,18 +1,12 @@
 package net.dubi.tutorialmod;
 
+import net.dubi.tutorialmod.block.ModBlocks;
 import net.dubi.tutorialmod.item.ModItems;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -32,6 +26,7 @@ public class TutorialMod {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
